2be992d17b7027d37a822549836b856aafb59c61
Pete King <peterjking@gmail.com>
2021-06-03 16:15:42 +0100

    Initial IaC with Pulumi

---
+++ src/Samples.WeatherForecast.Infra/AzureStack.cs
@@ -0,0 +1,72 @@
+using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
+
+class AzureStack : Stack
+{
+    public AzureStack()
+    {
+        var config = new Pulumi.Config();
+        
+        var dockerImage = config.Require("docker-image");
+
+        // Resource Group
+        var rg = new ResourceGroup("rg-weatherforecastapi-uks-");
+
+        // AppService Plan
+        var appServicePlan = new AppServicePlan("appplan-weatherforecastapi-uks-", new AppServicePlanArgs() {
+            ResourceGroupName = rg.Name,
+            Location = rg.Location,
+            Kind = "Linux",
+            Reserved = true,
+            Sku = new SkuDescriptionArgs
+            {
+                Name = "B1",
+                Tier = "BASIC"
+            },
+        });
+
+        // Current Docker image - this should be the location of your image.
+        // This will need to be dynamically set when we integrate with GitHub Actions.
+        //var dockerImage = "";
+
+        // WebApp for Containers
+        var app = new WebApp("app-weatherforecastapi-uks-", new WebAppArgs() 
+        { 
+            ResourceGroupName = rg.Name,
+            ServerFarmId = appServicePlan.Id,
+            SiteConfig = new SiteConfigArgs
+            {
+                AppSettings = new[] 
+                { 
+                    new NameValuePairArgs
+                    {
+                        Name = "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
+                        Value = "false"
+                    },
+                    new NameValuePairArgs
+                    {
+                        Name = "DOCKER_REGISTRY_SERVER_URL",
+                        Value = "https://ghcr.io"
+                    },
+                    new NameValuePairArgs
+                    {
+                        Name = "WEBSITES_PORT",
+                        Value = "8080" // Our custom image exposes port 80. Adjust for your app as needed.
+                    }
+                },
+                AlwaysOn = true,
+                LinuxFxVersion = $"DOCKER|{dockerImage}"
+            },
+            HttpsOnly = true
+        });
+
+        this.Endpoint = Output.Format($"https://{app.DefaultHostName}/weatherforecast");
+        this.HealthEndpoint = Output.Format($"https://{app.DefaultHostName}/health"); 
+    }
+
+    [Output] public Output<string> Endpoint { get; set; }
+
+    [Output] public Output<string> HealthEndpoint { get; set; }
+}
